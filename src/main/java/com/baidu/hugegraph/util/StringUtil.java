1cee1906fce0a795cdebce19b41fff3cc20fe9f0
Liu Jie <liujie23@baidu.com>
2017-05-25 14:34:11 +0800

    At descSchema(), using StringBuilder instead of concatenation to imporve the performance since it much faster and with minimum memory consumption.
    HugeGraph-152
    
    Change-Id: Icfc0b965895682de91a51d7d3a37447823a7b0ca
    Signed-off-by: Liu Jie <liujie23@baidu.com>

--- src/main/java/com/baidu/hugegraph/util/StringUtil.java
+++ src/main/java/com/baidu/hugegraph/util/StringUtil.java
@@ -8,31 +8,33 @@ import com.google.common.base.Preconditions;
  * Created by liningrui on 2017/3/22.
  */
 public class StringUtil {
-
     public static String descSchema(String prefix, Set<String> elems) {
-        String desc = "";
+        StringBuilder desc = new StringBuilder();
+
         if (elems != null) {
-            desc += ".";
-            desc += prefix;
-            desc += "(";
+            desc.append(".").append(prefix).append("(");
             for (String elem : elems) {
-                desc += "\"";
-                desc += elem;
-                desc += "\",";
+                desc.append("\"").append(elem).append("\",");
             }
-            int endIdx = desc.lastIndexOf(",") > 0 ? desc.length() - 1 : desc.length();
-            desc = desc.substring(0, endIdx) + ")";
+            int endIdx = desc.lastIndexOf(",") > 0 ?
+                         desc.length() - 1 : desc.length();
+
+            desc = desc.substring(0, endIdx).append(")");
         }
-        return desc;
+
+        return desc.toString();
     }
 
     public static void checkName(String name) {
-        Preconditions.checkNotNull(name, "name can not be null.");
-        Preconditions.checkNotNull(!name.isEmpty(), "name can not be empty.");
-        Preconditions.checkArgument(name.length() < 256, "the length of name must less than 256.");
-        Preconditions.checkArgument(name.substring(0, 1) != "_", "The first letter of name can not be '_'.");
-        Preconditions.checkArgument(!name.contains("\u0001"), "name can not contain the character '\u0001'.");
-        Preconditions.checkArgument(!name.contains("\u0002"), "name can not contain the character '\u0002'.");
+        Preconditions.checkNotNull(name, "name can't be null.");
+        Preconditions.checkNotNull(!name.isEmpty(), "name can't be empty.");
+        Preconditions.checkArgument(name.length() < 256,
+                "The length of name must less than 256 bytes.");
+        Preconditions.checkArgument(name.substring(0, 1) != "_",
+                "The first letter of name can't be '_'.");
+        Preconditions.checkArgument(!name.contains("\u0001"),
+                "name can't contain the character '\u0001'.");
+        Preconditions.checkArgument(!name.contains("\u0002"),
+                "name can't contain the character '\u0002'.");
     }
-
 }
