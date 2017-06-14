661f559099e3e1bf83b36fdff6979ef30051b391
zhangsuochao <zhangsuochao@baidu.com>
2017-06-14 14:10:49 +0800

    HugeGraph-270:Deploy jar to baidu repository

--- build.sh
+++ build.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
-export MAVEN_HOME=/home/scmtools/buildkit/maven/apache-maven-3.3.9/
-export JAVA_HOME=/home/scmtools/buildkit/java/jdk1.8.0_25/
-export PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
+
+HUGEGRAPH_CLIENT_RELEASE_PATH="${PWD}/output/"
+
+export MAVEN_HOME="/home/scmtools/buildkit/maven/apache-maven-3.3.9/"
+export JAVA_HOME="/home/scmtools/buildkit/java/jdk1.8.0_25/"
+export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"
+
 mvn clean package -DskipTests
-mvn deploy
+
+if [ -d $HUGEGRAPH_CLIENT_RELEASE_PATH ]; then
+	echo "$HUGEGRAPH_CLIENT_RELEASE_PATH is already exists."
+	exit 1
+fi
+
+mkdir -p $HUGEGRAPH_CLIENT_RELEASE_PATH || echo "Failed to create directory: $HUGEGRAPH_CLIENT_RELEASE_PATH"
+
+cp target/*.jar $HUGEGRAPH_CLIENT_RELEASE_PATH
+if [ $? -ne 0 ]; then
+	echo "Move jar to $HUGEGRAPH_CLIENT_RELEASE_PATH failed."
+else
+	echo "Build Succeed!"
+fi
+
+echo "Deploy to baidu maven repository..."
+
+mvn deploy -DskipTests
+
+exit $?
