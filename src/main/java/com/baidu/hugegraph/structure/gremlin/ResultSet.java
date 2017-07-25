e18c09af0b38c057a3bc9ccc9c3b29cd522c3812
liningrui <liningrui@baidu.com>
2017-07-25 16:48:07 +0800

    HugeGraph-508: Add license on hugegraph client code.
    Change-Id: I1115ee68c7077f145500d15f4a65694647eeb90f

--- src/main/java/com/baidu/hugegraph/structure/gremlin/ResultSet.java
+++ src/main/java/com/baidu/hugegraph/structure/gremlin/ResultSet.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.baidu.hugegraph.structure.gremlin;
 
 import java.util.Iterator;
@@ -13,9 +32,7 @@ import com.baidu.hugegraph.structure.graph.Vertex;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-/**
- * Created by liningrui on 2017/6/25.
- */
+
 public class ResultSet {
 
     @JsonProperty
