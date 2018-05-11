a2daca9ccff55f6d31f792878a311c3c55a0b3f2
gerrit <gerrit@baidu.com>
2018-05-11 15:31:44 +0800

    init

---
+++ postBuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##########################################################################################
+## Do something after maven build.
+##
+## Include the following content:
+## - Change directory
+## - Delete some temporary files or directories
+## - Create a output directory
+## - Move files
+##########################################################################################
