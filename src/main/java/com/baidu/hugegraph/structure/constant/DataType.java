9ad2a46345f4c3f92c0f5f788c236c47f3baf44d
liningrui <liningrui@baidu.com>
2018-06-08 11:39:12 +0800

    HugeGraph-1300: HugeClient extent DataType class
    Change-Id: Ia9c6036857b45da50c517747286d66ebf3677362

--- src/main/java/com/baidu/hugegraph/structure/constant/DataType.java
+++ src/main/java/com/baidu/hugegraph/structure/constant/DataType.java
@@ -19,27 +19,33 @@
 
 package com.baidu.hugegraph.structure.constant;
 
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
 public enum DataType {
 
-    OBJECT(1, "object"),
-    BOOLEAN(2, "boolean"),
-    BYTE(3, "byte"),
-    INT(4, "int"),
-    LONG(5, "long"),
-    FLOAT(6, "float"),
-    DOUBLE(7, "double"),
-    TEXT(8, "text"),
-    BLOB(9, "blob"),
-    DATE(10, "date"),
-    UUID(11, "uuid");
+    OBJECT(1, "object", Serializable.class),
+    BOOLEAN(2, "boolean", Boolean.class),
+    BYTE(3, "byte", Byte.class),
+    INT(4, "int", Integer.class),
+    LONG(5, "long", Long.class),
+    FLOAT(6, "float", Float.class),
+    DOUBLE(7, "double", Double.class),
+    TEXT(8, "text", String.class),
+    BLOB(9, "blob", byte[].class),
+    DATE(10, "date", Date.class),
+    UUID(11, "uuid", UUID.class);
 
     private byte code = 0;
     private String name = null;
+    private Class<?> clazz = null;
 
-    DataType(int code, String name) {
+    DataType(int code, String name, Class<?> clazz) {
         assert code < 256;
         this.code = (byte) code;
         this.name = name;
+        this.clazz = clazz;
     }
 
     public byte code() {
@@ -50,4 +56,20 @@ public enum DataType {
         return this.name;
     }
 
+    public Class<?> clazz() {
+        return this.clazz;
+    }
+
+    public boolean isNumber() {
+        return this == BYTE || this == INT || this == LONG ||
+               this == FLOAT || this == DOUBLE;
+    }
+
+    public boolean isDate() {
+        return this == DataType.DATE;
+    }
+
+    public boolean isUUID() {
+        return this == DataType.UUID;
+    }
 }
