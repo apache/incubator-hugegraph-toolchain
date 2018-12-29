5980bc7cea166a573402f42ea51b9f110236a156
liningrui <liningrui@baidu.com>
2018-12-29 16:23:39 +0800

    Support load with a specified directory
    Implement #29
    
    Change-Id: Id3d9385b52484a30d6e71652ef1b55c40979dd88

--- src/main/java/com/baidu/hugegraph/loader/reader/file/FileReader.java
+++ src/main/java/com/baidu/hugegraph/loader/reader/file/FileReader.java
@@ -28,6 +28,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
@@ -35,24 +37,25 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 
 import com.baidu.hugegraph.loader.exception.LoadException;
+import com.baidu.hugegraph.loader.exception.ReadException;
 import com.baidu.hugegraph.loader.reader.InputReader;
 import com.baidu.hugegraph.loader.source.file.FileSource;
 import com.baidu.hugegraph.util.Log;
 
 public abstract class FileReader implements InputReader {
 
-    private Logger LOG = Log.logger(FileReader.class);
+    private static Logger LOG = Log.logger(FileReader.class);
 
     private static final int BUF_SIZE = 5 * 1024 * 1024;
 
     private final FileSource source;
-    private final BufferedReader reader;
+    private final BufferedReaderWrapper readers;
     private String nextLine;
 
     public FileReader(FileSource source) {
         this.source = source;
         try {
-            this.reader = this.open(source);
+            this.readers = this.open(source);
         } catch (IOException e) {
             throw new LoadException("Failed to load input file '%s'",
                                     e, source.path());
@@ -72,7 +75,7 @@ public abstract class FileReader implements InputReader {
     public boolean hasNext() {
         if (this.nextLine == null) {
             try {
-                this.nextLine = this.reader.readLine();
+                this.nextLine = this.readers.readNextLine();
             } catch (IOException e) {
                 throw new LoadException("Read next line error", e);
             }
@@ -97,17 +100,29 @@ public abstract class FileReader implements InputReader {
 
     @Override
     public void close() throws IOException {
-        this.reader.close();
+        this.readers.close();
     }
 
     protected abstract Map<String, Object> transform(String line);
 
-    private BufferedReader open(FileSource source) throws IOException {
+    private BufferedReaderWrapper open(FileSource source) throws IOException {
+        String path = source.path();
+        File file = FileUtils.getFile(path);
+        checkFileOrDir(file);
+
+        if (file.isFile()) {
+            return new BufferedReaderWrapper(source, file);
+        } else {
+            assert file.isDirectory();
+            return new BufferedReaderWrapper(source, file.listFiles());
+        }
+    }
+
+    private static BufferedReader createBufferedFileReader(FileSource source,
+                                                           File file)
+                                                           throws IOException {
         String path = source.path();
         String charset = source.charset();
-
-        File file = FileUtils.getFile(path);
-        checkFile(file);
 
         InputStream fis = null;
         try {
@@ -130,17 +145,81 @@ public abstract class FileReader implements InputReader {
         return this.source.commentSymbols().stream().anyMatch(line::startsWith);
     }
 
-    private static void checkFile(File file) {
+    private static void checkFileOrDir(File file) {
         if (!file.exists()) {
-            throw new LoadException("Please ensure the file exist: '%s'", file);
-        }
-        if (!file.isFile()) {
-            throw new LoadException("Please ensure the file is indeed a file " +
-                                    "instead of a directory: '%s'", file);
+            throw new LoadException(
+                      "Please ensure the file or directory exist: '%s'", file);
         }
         if (!file.canRead()) {
-            throw new LoadException("Please ensure the file is readable: '%s'",
-                                    file);
+            throw new LoadException(
+                      "Please ensure the file or directory is readable: '%s'",
+                      file);
+        }
+    }
+
+    private static class BufferedReaderWrapper {
+
+        private final FileSource source;
+        private final List<File> files;
+
+        private int index;
+        private BufferedReader reader;
+
+        public BufferedReaderWrapper(FileSource source, File... files) {
+            this.source = source;
+            this.files = Arrays.asList(files);
+            this.index = 0;
+            if (files.length == 0) {
+                this.reader = null;
+            } else {
+                this.reader = this.openFile(this.index);
+            }
+        }
+
+        private BufferedReader openFile(int index) {
+            assert index < this.files.size();
+            File file = this.files.get(index);
+            try {
+                LOG.info("Ready to open file '{}'", file.getName());
+                return createBufferedFileReader(this.source, file);
+            } catch (IOException e) {
+                throw new ReadException(file.getAbsolutePath(),
+                          "Failed to create file reader for file '%s'",
+                          file.getName());
+            }
+        }
+
+        private void closeFile(int index) throws IOException {
+            assert index < this.files.size();
+            File file = this.files.get(index);
+            LOG.info("Ready to close file '{}'", file.getName());
+            this.close();
+        }
+
+        public String readNextLine() throws IOException {
+            // reader is null means there is no file
+            if (this.reader == null) {
+                return null;
+            }
+
+            String line;
+            while ((line = this.reader.readLine()) == null) {
+                // The current file is read at the end, ready to read next one
+                this.closeFile(this.index);
+
+                if (++this.index < this.files.size()) {
+                    this.reader = this.openFile(this.index);
+                } else {
+                    return null;
+                }
+            }
+            return line;
+        }
+
+        public void close() throws IOException {
+            if (this.reader != null) {
+                this.reader.close();
+            }
         }
     }
 }
