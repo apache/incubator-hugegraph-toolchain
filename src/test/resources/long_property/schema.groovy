89e157ebf0fce36178b709f07cef3a457714c513
liningrui <liningrui@baidu.com>
2019-08-29 14:34:06 +0800

    fix schema error
    Change-Id: Ia5b8272c3f486b112ccf4fc4e9ce8928c1948ca9

--- src/test/resources/long_property/schema.groovy
+++ src/test/resources/long_property/schema.groovy
@@ -2,7 +2,7 @@
 schema.propertyKey("name").asText().ifNotExist().create();
 schema.propertyKey("age").asInt().ifNotExist().create();
 schema.propertyKey("city").asText().ifNotExist().create();
-schema.propertyKey("weight").asDouble().ifNotExist().create();
+schema.propertyKey("weight").asLong().ifNotExist().create();
 schema.propertyKey("date").asText().ifNotExist().create();
 
 schema.vertexLabel("person").properties("name", "age", "city", "weight").primaryKeys("name").ifNotExist().create();
