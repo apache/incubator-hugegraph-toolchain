d1df6780a0d196475245a0ea8a375eb21f3dba85
liningrui <liningrui@baidu.com>
2021-03-08 11:52:42 +0800

    test install hdfs
    Change-Id: Idfdcba4b04480294cf648232a95930ab885a743b

--- assembly/travis/install-hadoop.sh
+++ assembly/travis/install-hadoop.sh
@@ -2,22 +2,22 @@
 
 set -ev
 
-UBUNTU_CODENAME=$(lsb_release -c | awk '{print $2}')
+sudo wget http://archive.apache.org/dist/hadoop/common/hadoop-2.8.5/hadoop-2.8.5.tar.gz
 
-sudo tee /etc/apt/sources.list.d/cdh.list <<EOF
-deb [arch=amd64] http://archive.cloudera.com/cdh5/ubuntu/${UBUNTU_CODENAME}/amd64/cdh ${UBUNTU_CODENAME}-cdh5 contrib
-EOF
+sudo tar -zxf hadoop-2.8.5.tar.gz -C /usr/local
+cd /usr/local
+sudo mv hadoop-2.8.5 hadoop
+sudo chown -R travis ./hadoop
+cd hadoop
+pwd
 
-sudo tee /etc/apt/preferences.d/cloudera.pref <<EOF
-Package: *
-Pin: release o=Cloudera, l=Cloudera
-Pin-Priority: 501
-EOF
+echo "export HADOOP_HOME=/usr/local/hadoop" >> ~/.bashrc
+echo "export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native" >> ~/.bashrc
+echo "export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin" >> ~/.bashrc
 
-sudo apt-get update
+source ~/.bashrc
 
-sudo mkdir -p /etc/hadoop/conf.gohdfs
-sudo tee /etc/hadoop/conf.gohdfs/core-site.xml <<EOF
+tee etc/hadoop/core-site.xml <<EOF
 <configuration>
     <property>
         <name>fs.defaultFS</name>
@@ -26,7 +26,7 @@ sudo tee /etc/hadoop/conf.gohdfs/core-site.xml <<EOF
 </configuration>
 EOF
 
-sudo tee /etc/hadoop/conf.gohdfs/hdfs-site.xml <<EOF
+tee etc/hadoop/hdfs-site.xml <<EOF
 <configuration>
     <property>
         <name>dfs.namenode.name.dir</name>
@@ -47,16 +47,7 @@ sudo tee /etc/hadoop/conf.gohdfs/hdfs-site.xml <<EOF
 </configuration>
 EOF
 
-sudo update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.gohdfs 99
-sudo apt-get install -y --allow-unauthenticated hadoop-hdfs-namenode hadoop-hdfs-datanode
-
-sudo mkdir -p /opt/hdfs/data /opt/hdfs/name
-sudo chown -R hdfs:hdfs /opt/hdfs
-sudo -u hdfs hdfs namenode -format -nonInteractive
-
-sudo adduser travis hadoop
-
-sudo service hadoop-hdfs-datanode restart
-sudo service hadoop-hdfs-namenode restart
-
-hdfs dfsadmin -safemode wait
+bin/hdfs namenode -format
+sbin/hadoop-daemon.sh start namenode
+sbin/hadoop-daemon.sh start datanode
+jps
