5e3be0b4bf322579a5e2cadb34840da770029e21
imbajin <jin@apache.org>
2022-04-28 18:40:28 +0800

    remove ojdbc in root & use mvn -e

--- hugegraph-hubble/hubble-dist/assembly/travis/install-hugegraph-hubble.sh
+++ hugegraph-hubble/hubble-dist/assembly/travis/install-hugegraph-hubble.sh
@@ -4,9 +4,10 @@ set -ev
 
 TRAVIS_DIR=$(dirname "$0")
 
-pwd && mvn -e clean package -DskipTests
+echo "$TRAVIS_DIR"
+pwd && mvn -e -X clean package -DskipTests
 
-cp "${TRAVIS_DIR}"/jacocoagent.jar hugegraph-hubble-*/lib
-cp "${TRAVIS_DIR}"/start-hubble.sh hugegraph-hubble-*/bin
+cp "${TRAVIS_DIR}"/jacocoagent.jar hugegraph-hubble-*/lib || exit 1
+cp "${TRAVIS_DIR}"/start-hubble.sh hugegraph-hubble-*/bin || exit 1
 
 hugegraph-hubble-*/bin/start-hubble.sh
