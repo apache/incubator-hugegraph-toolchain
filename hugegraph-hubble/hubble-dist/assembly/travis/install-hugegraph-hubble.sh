3ee7490baff7b2c48c7c8b18609bfacdae1d05df
Simon Cheung <ming@apache.org>
2022-07-22 17:57:05 +0800

    feat: add hubble dockerfile (#295)

--- hugegraph-hubble/hubble-dist/assembly/travis/install-hugegraph-hubble.sh
+++ hugegraph-hubble/hubble-dist/assembly/travis/install-hugegraph-hubble.sh
@@ -3,7 +3,7 @@
 set -ev
 
 TRAVIS_DIR=$(dirname "$0")
-HUBBLE_DIR="hugegraph-hubble-*.*"
+HUBBLE_DIR="hugegraph-hubble"
 
 echo "$TRAVIS_DIR"
 pwd && mvn -e -X clean package -DskipTests
@@ -11,4 +11,4 @@ pwd && mvn -e -X clean package -DskipTests
 cp "${TRAVIS_DIR}"/jacocoagent.jar $HUBBLE_DIR/lib || exit 1
 cp "${TRAVIS_DIR}"/start-hubble.sh $HUBBLE_DIR/bin || exit 1
 
-./hugegraph-hubble-*/bin/start-hubble.sh
+${HUBBLE_DIR}/bin/start-hubble.sh
