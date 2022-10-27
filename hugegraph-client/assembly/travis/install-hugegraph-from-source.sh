6c2f2690847ca87fa2629686af5b9b862460a033
z7658329 <1043706593@qq.com>
2022-10-27 16:55:57 +0800

    fix tools ci fail

--- hugegraph-client/assembly/travis/install-hugegraph-from-source.sh
+++ hugegraph-client/assembly/travis/install-hugegraph-from-source.sh
@@ -33,10 +33,7 @@ cp -rf "${TRAVIS_DIR}"/conf/. "${HTTP_SERVER_DIR}"/conf/
 # start HugeGraphServer with http protocol
 cd "${HTTP_SERVER_DIR}"
 echo -e "pa" | bin/init-store.sh || exit 1
-nohup bin/start-hugegraph.sh > out.log 2>&1 &
-sleep 10
-cat out.log
-cat /home/runner/work/incubator-hugegraph-toolchain/incubator-hugegraph-toolchain/hugegraph-0.13.0/logs/hugegraph-server.log
+bin/start-hugegraph.sh || exit 1
 
 # config options for https server
 cd ../"${HTTPS_SERVER_DIR}"
