9041c326b6da811136f717962ffcf1aa5ff60a08
imbajin <jin@apache.org>
2022-11-01 22:14:35 +0800

    Update Line.java

--- hugegraph-loader/src/main/java/com/baidu/hugegraph/loader/reader/line/Line.java
+++ hugegraph-loader/src/main/java/com/baidu/hugegraph/loader/reader/line/Line.java
@@ -37,10 +37,11 @@ public final class Line {
         E.checkArgumentNotNull(rawLine, "The rawLine can't be null");
         E.checkArgumentNotNull(names, "The names can't be null");
         E.checkArgumentNotNull(values, "The values can't be null");
-        E.checkArgument(names.length == values.length,
-                "The length of names %s should be same as values %s",
-                Arrays.toString(names),
-                Arrays.toString(values));
+        if (names.length != values.length) {
+            E.checkArgument(names.length == values.length,
+                            "The length of names %s should be same as values %s",
+                            Arrays.toString(names), Arrays.toString(values));
+        }
         this.rawLine = rawLine;
         this.names = names;
         this.values = values;
