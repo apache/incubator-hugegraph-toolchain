af01011dd9761cd9446498f4b4c8347141732c42
imbajin <jin@apache.org>
2023-01-12 22:03:29 +0800

    refact: enhance shell script style (#418)

--- hugegraph-loader/assembly/static/bin/hugegraph-flinkcdc-loader.sh
+++ hugegraph-loader/assembly/static/bin/hugegraph-flinkcdc-loader.sh
@@ -16,20 +16,20 @@
 # limitations under the License.
 #
 BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-APP_DIR=$(dirname ${BIN_DIR})
+APP_DIR=$(dirname "${BIN_DIR}")
 LIB_DIR=${APP_DIR}/lib
-ASSEMBLY_JAR_NAME=$(find ${LIB_DIR} -name hugegraph-loader*.jar)
+ASSEMBLY_JAR_NAME=$(find "${LIB_DIR}" -name 'hugegraph-loader*.jar')
 
 # get hugegraph_params and engine_params
 source "$BIN_DIR"/get-params.sh
-get_params $*
+get_params "$@"
 echo "engine_params: $ENGINE_PARAMS"
 echo "hugegraph_params: $HUGEGRAPH_PARAMS"
 
 CMD=${FLINK_HOME}/bin/flink run \
-  ${ENGINE_PARAMS} \
-  -c org.apache.hugegraph.loader.flink.HugeGraphFlinkCDCLoader \
-  ${ASSEMBLY_JAR_NAME} ${HUGEGRAPH_PARAMS}
+    "${ENGINE_PARAMS}" \
+    -c org.apache.hugegraph.loader.flink.HugeGraphFlinkCDCLoader \
+    "${ASSEMBLY_JAR_NAME}" "${HUGEGRAPH_PARAMS}"
 
-echo ${CMD}
-exec ${CMD}
+echo "${CMD}"
+exec "${CMD}"
--- hugegraph-loader/assembly/static/bin/hugegraph-spark-loader.sh
+++ hugegraph-loader/assembly/static/bin/hugegraph-spark-loader.sh
@@ -16,25 +16,23 @@
 # limitations under the License.
 #
 BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-APP_DIR=$(dirname ${BIN_DIR})
+APP_DIR=$(dirname "${BIN_DIR}")
 LIB_DIR=${APP_DIR}/lib
 
 # get hugegraph_params and engine_params
 source "$BIN_DIR"/get-params.sh
-get_params $*
+get_params "$@"
 echo "engine_params: $ENGINE_PARAMS"
 echo "hugegraph_params: $HUGEGRAPH_PARAMS"
 
-ASSEMBLY_JAR_NAME=$(find ${LIB_DIR} -name hugegraph-loader*.jar)
-
+ASSEMBLY_JAR_NAME=$(find "${LIB_DIR}" -name 'hugegraph-loader*.jar')
 
 DEFAULT_APP_NAME="hugegraph-spark-loader"
 APP_NAME=${APP_NAME:-$DEFAULT_APP_NAME}
 
 CMD="${SPARK_HOME}/bin/spark-submit
-    --class org.apache.hugegraph.loader.spark.HugeGraphSparkLoader \
-    ${ENGINE_PARAMS}
-    --jars $(echo ${LIB_DIR}/*.jar | tr ' ' ',') ${ASSEMBLY_JAR_NAME} ${HUGEGRAPH_PARAMS}"
+    --class org.apache.hugegraph.loader.spark.HugeGraphSparkLoader ${ENGINE_PARAMS} \
+    --jars $(echo "${LIB_DIR}"/*.jar | tr ' ' ',') ${ASSEMBLY_JAR_NAME} ${HUGEGRAPH_PARAMS}"
 
-echo ${CMD}
-exec ${CMD}
+echo "${CMD}"
+exec "${CMD}"
