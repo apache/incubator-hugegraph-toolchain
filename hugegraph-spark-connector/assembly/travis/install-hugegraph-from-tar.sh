7dd0259033cf98fe2b00e10d2c079e51ee3abb42
liuxiao <liuxiao2103@qq.com>
2023-09-25 01:32:54 +0800

    update test env

--- hugegraph-spark-connector/assembly/travis/install-hugegraph-from-tar.sh
+++ hugegraph-spark-connector/assembly/travis/install-hugegraph-from-tar.sh
@@ -24,29 +24,12 @@ fi
 VERSION=$1
 HUGEGRAPH_LINK="https://downloads.apache.org/incubator/hugegraph/${VERSION}/apache-hugegraph-incubating-${VERSION}.tar.gz"
 
-wget "${HUGEGRAPH_LINK}" || exit 1
-# TODO: lack incubator after apache package release (update it later)
-mv apache-hugegraph-incubating-${VERSION}.tar.gz hugegraph-${VERSION}.tar.gz
-tar zxvf hugegraph-${VERSION}.tar.gz
+wget "${HUGEGRAPH_LINK}" -q --show-progress || exit 1
+tar zxvf apache-hugegraph-incubating-${VERSION}.tar.gz
+cd apache-hugegraph-incubating-${VERSION}
 
-HTTPS_SERVER_DIR="hugegraph_https"
-mkdir ${HTTPS_SERVER_DIR}
-# TODO: lack incubator after apache package release (update it later)
-cp -r hugegraph-${VERSION}/. ${HTTPS_SERVER_DIR}
-cd hugegraph-${VERSION}
-
-# start HugeGraphServer with http protocol
-bin/init-store.sh || exit 1
-bin/start-hugegraph.sh || exit 1
-
-cd ../${HTTPS_SERVER_DIR}
-REST_SERVER_CONFIG="conf/rest-server.properties"
-GREMLIN_SERVER_CONFIG="conf/gremlin-server.yaml"
-sed -i "s?http://127.0.0.1:8080?https://127.0.0.1:8443?g" "$REST_SERVER_CONFIG"
-sed -i "s/#port: 8182/port: 8282/g" "$GREMLIN_SERVER_CONFIG"
-echo "gremlinserver.url=http://127.0.0.1:8282" >> $REST_SERVER_CONFIG
-
-# start HugeGraphServer with https protocol
+## start HugeGraphServer with http protocol
 bin/init-store.sh
 bin/start-hugegraph.sh
+
 cd ../
