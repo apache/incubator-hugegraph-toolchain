c2bf006159dea118bbe8c42fdcab63a299f67e71
liuxiao <liuxiao2103@qq.com>
2023-09-25 00:32:34 +0800

    fix: chmod sh in windows

--- hugegraph-spark-connector/assembly/travis/install-hugegraph-from-source.sh
+++ hugegraph-spark-connector/assembly/travis/install-hugegraph-from-source.sh
@@ -47,7 +47,6 @@ cd ../${HTTPS_SERVER_DIR}
 REST_SERVER_CONFIG="conf/rest-server.properties"
 GREMLIN_SERVER_CONFIG="conf/gremlin-server.yaml"
 sed -i "s?http://127.0.0.1:8080?https://127.0.0.1:8443?g" "$REST_SERVER_CONFIG"
-sed -i "s/rpc.server_port=8091/rpc.server_port=8092/g" "$REST_SERVER_CONFIG"
 sed -i "s/#port: 8182/port: 8282/g" "$GREMLIN_SERVER_CONFIG"
 echo "gremlinserver.url=http://127.0.0.1:8282" >> ${REST_SERVER_CONFIG}
 
