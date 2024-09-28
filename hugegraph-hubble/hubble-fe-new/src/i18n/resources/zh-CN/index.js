acd6948479f4ed698be8a59567f79309c1a7153d
FrostyHec <112929809+FrostyHec@users.noreply.github.com>
2024-09-28 18:09:09 +0800

    fix i18n display

--- hugegraph-hubble/hubble-fe-new/src/i18n/resources/zh-CN/index.js
+++ hugegraph-hubble/hubble-fe-new/src/i18n/resources/zh-CN/index.js
@@ -7,6 +7,7 @@ import {
 import {
     Home,
     Manage,
+    Analysis,
 } from './modules';
 
 const translation = {
@@ -15,7 +16,8 @@ const translation = {
         Common,
         Home,
         Manage,
-        ERView
+        ERView,
+        Analysis
     ),
 };
 
--- hugegraph-hubble/hubble-fe-new/src/i18n/resources/zh-CN/modules/index.js
+++ hugegraph-hubble/hubble-fe-new/src/i18n/resources/zh-CN/modules/index.js
@@ -1,6 +1,8 @@
 import Home from './home.json';
 import Manage from './manage.json';
+import Analysis from './analysis.json';
 export {
     Home,
+    Analysis,
     Manage,
 };
