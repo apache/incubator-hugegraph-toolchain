c672e356be717b8db810d264b289b40e9c6e1343
John <thespica@qq.com>
2025-02-20 18:58:15 +0800

    chore(client): patch for father sub edge (#654)
    ---------
    
    Co-authored-by: imbajin <jin@apache.org>
    Co-authored-by: VGalaxies <dyc1904821183@gmail.com>

--- hugegraph-client/src/main/java/org/apache/hugegraph/structure/constant/EdgeLabelType.java
+++ hugegraph-client/src/main/java/org/apache/hugegraph/structure/constant/EdgeLabelType.java
@@ -23,10 +23,12 @@ public enum EdgeLabelType {
 
     PARENT(1, "PARENT"),
 
-    SUB(2, "SUB");
+    SUB(2, "SUB"),
 
-    private final byte code;
-    private final String name;
+    GENERAL(3, "GENERAL");
+
+    private byte code = 0;
+    private String name = null;
 
     EdgeLabelType(int code, String name) {
         assert code < 256;
@@ -44,6 +46,10 @@ public enum EdgeLabelType {
 
     public boolean normal() {
         return this == NORMAL;
+    }
+
+    public boolean general() {
+        return this == GENERAL;
     }
 
     public byte code() {
