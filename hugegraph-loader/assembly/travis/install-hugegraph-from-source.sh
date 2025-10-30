0b61e7c11b0136e61a7636011a90e91c742cf4ec
sadwitdastreetz <ddgerm@outlook.com>
2025-10-31 00:16:52 +0800

    rebuild loader-ci

--- hugegraph-loader/assembly/travis/install-hugegraph-from-source.sh
+++ hugegraph-loader/assembly/travis/install-hugegraph-from-source.sh
@@ -51,7 +51,9 @@ cd ../${HTTPS_SERVER_DIR}
 REST_SERVER_CONFIG="conf/rest-server.properties"
 GREMLIN_SERVER_CONFIG="conf/gremlin-server.yaml"
 sed -i "s?http://127.0.0.1:8080?https://127.0.0.1:8443?g" "$REST_SERVER_CONFIG"
-sed -i "s/rpc.server_port=8091/rpc.server_port=8092/g" "$REST_SERVER_CONFIG"
+sed -i "s/#rpc.server_port=8091/rpc.server_port=8092/g" "$REST_SERVER_CONFIG"
+sed -i "s/#rpc.server_host=127.0.0.1/rpc.server_host=127.0.0.1/g" "$REST_SERVER_CONFIG"
+sed -i "s/#rpc.server_timeout=30/rpc.server_timeout=30/g" "$REST_SERVER_CONFIG"
 sed -i "s/#port: 8182/port: 8282/g" "$GREMLIN_SERVER_CONFIG"
 echo "gremlinserver.url=http://127.0.0.1:8282" >> ${REST_SERVER_CONFIG}
 
