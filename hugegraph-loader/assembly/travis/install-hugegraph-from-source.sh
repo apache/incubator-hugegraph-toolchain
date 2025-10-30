5259c4bf39b5a6f7984a36f8304d7205fbd12758
sadwitdastreetz <ddgerm@outlook.com>
2025-10-30 22:18:50 +0800

    rebuild loader-ci

--- hugegraph-loader/assembly/travis/install-hugegraph-from-source.sh
+++ hugegraph-loader/assembly/travis/install-hugegraph-from-source.sh
@@ -41,9 +41,9 @@ mkdir ${HTTPS_SERVER_DIR}
 cp -r apache-hugegraph-*/. ${HTTPS_SERVER_DIR}
 cd "$(find apache-hugegraph-* | head -1)"
 # start HugeGraphServer with http protocol
-bin/init-store.sh || exit 1
 sed -i 's|#auth.authenticator=.*|auth.authenticator=org.apache.hugegraph.auth.StandardAuthenticator|' conf/rest-server.properties
 sed -i 's|#auth.admin_pa=.*|auth.admin_pa=pa|' conf/rest-server.properties
+bin/init-store.sh || exit 1
 bin/start-hugegraph.sh || exit 1
 
 cd ../${HTTPS_SERVER_DIR}
@@ -55,8 +55,8 @@ sed -i "s/#port: 8182/port: 8282/g" "$GREMLIN_SERVER_CONFIG"
 echo "gremlinserver.url=http://127.0.0.1:8282" >> ${REST_SERVER_CONFIG}
 
 # start HugeGraphServer with https protocol
-bin/init-store.sh
 sed -i 's|#auth.authenticator=.*|auth.authenticator=org.apache.hugegraph.auth.StandardAuthenticator|' conf/rest-server.properties
 sed -i 's|#auth.admin_pa=.*|auth.admin_pa=pa|' conf/rest-server.properties
+bin/init-store.sh
 bin/start-hugegraph.sh
 cd ../
